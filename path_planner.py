2311ec2f38bda456545d194c48242a846f2a96d8
tory-lena <victoriam.dax@gmail.com>
2017-05-28 18:39:00 -0700

    Update path_planner.py

--- path_planner.py
+++ path_planner.py
@@ -1,13 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from simulator import *
+from path_controller import *
 import pdb
+
+def plan(mission, x):
+	n=len(mission)
+	if n==3: path=brute_force(mission, x)
+	if n==4: path=DP_dyn_sort(mission, x)
+	if n==5: path=hybrid(mission, x)
+	
+	return path
+	
 
 
 #----------------DP
 # input: mission as a list of arrays
 #        x - state of plane (array)
 # output: mission_sorted
-# variables: W matrix contrinaing all tge wieght for the DP alorithms
+# variables: W matrix contrinaing all the wieght for the DP alorithms
 
 
 def DP_sort(mission, x):
@@ -32,17 +43,21 @@ def DP_sort(mission, x):
 		path.insert(np.argmin(opt)+1, i)
 
 	return path
+	
+#return: path=list[(x,y,z) at t=computation_starts, (x,y,z) of mission point in optimized order]
+#computational cost: n! * (simulation+cost_function) + n (find min)
 
 def DP_dyn_sort(mission, x):
 
 	n=len(mission)
-	path=[x[:2]] #structure of path i is [mission points, cost]
+	path=[x[:2]]
 	cost=0.
 
 	for i in range(n):
 		i+=1 # cause 0 is starting point
 		opt=np.ones(i)
 		opt=opt*cost
+		
 		for j in range(i):
 			path_opt=path.insert(j+1, mission[i])
 			controller=PathController(path_opt)
@@ -60,7 +75,58 @@ def DP_dyn_sort(mission, x):
 		path.insert(np.argmin(opt)+1, mission[i])
 
 	return path
+	
+	
+def brute_force(mission, x):
 
+	if len(mission)!=3: break
+
+	cost=np.ones(6)
+	order=np.array([1,2,3;1,3,2;2,3,1;2,1,3;3,1,2;3,2,1])
+	for k in range(6):
+		path_opt=[mission[j-1] for j in order[k, :]]
+		path_opt=path_opt.insert(0,x[:2])
+		
+		controller=PathController(path_opt)
+		stats=rollout(controller.policy, x, 100, .1)
+		pos=np.vstack(stats["x"])
+		x=pos[:,0]
+		y=pos[:,1]
+		z=pos[:,2]
+		w=0.
+		for i in range(len(x)-1): 
+			w+=np.sqrt((x[i]-x[i+1])**2.+(y[i]-y[i+1])**2.+(z[i]-z[i+1])**2.)
+		cost[k]=w
+		
+	idx=min(cost)
+	path= [mission[j-1] for j in order(idx, :)]
+	
+	return path.insert(0, x[:2])
+	
+def hybrid(mission, x):
+
+	idx=DP_sort(mission, x)
+	cost=[]
+	IJK=np.array([2,2,3,3,4,4;3,4,2,4,2,3;4,3,4,2,3,2])
+	for i,j,k in IJK[0,:], IJK[1,:], IJK[2,:]:
+		path_opt=x[:2]+mission[idx[1]]+mission[i]+mission[j]+mission[k]+mission[-1] #init, closest point, ...,farest point
+		
+		controller=PathController(path_opt)
+		stats=rollout(controller.policy, x, 100, .1)
+		pos=np.vstack(stats["x"])
+		x=pos[:,0]
+		y=pos[:,1]
+		z=pos[:,2]
+		w=0.
+		for i in range(len(x)-1): 
+			w+=np.sqrt((x[i]-x[i+1])**2.+(y[i]-y[i+1])**2.+(z[i]-z[i+1])**2.)
+		cost.append(w)
+	
+	i=min(cost)
+	path=x[:2]+mission[idx[1]]+mission[[0,i]]+mission[IJK[1,i]]+mission[IJK[2,i]]+mission[-1]
+	
+	return path
+	
 
 #----------------------------------
 # input: mission as it was (only pts ahead, not the passed ones!), x is current state, new point new point

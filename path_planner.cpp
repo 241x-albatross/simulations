7cacfc4b2fee7cb250c1a096dfbd619a6e2b9e06
tory-lena <victoriam.dax@gmail.com>
2017-06-02 10:15:01 -0700

    error correction len(), start include penalty for high angles!

--- path_planner.cpp
+++ path_planner.cpp
@@ -5,12 +5,28 @@
 using namespace std
 
 //based on distance for now:
-double weight(double x2[], double x1[])
+double weight(double x1[], double x2[], double v, double x_prev[])
 {
-	double l1=x1[0]-x2[0];
-	double l2=x1[1]-x2[1];
-	w=sqrt(pow(l1,2)+pow(l2,2));
-	return w;
+	//distance eval
+	double l1=x2[0]-x1[0];
+	double l2=x2[1]-x1[1];
+	double v1[]={l1, l2};
+	w_d=sqrt(pow(l1,2)+pow(l2,2));
+	
+	const double pi= 3.141592653589793238462643383279502884;
+	
+	//angle
+	double v2[]={x_prev[0]-x1[0],x_prev[1]-x1[1]};
+	double theta=(180.0/pi)*acos(v1[0]*v2[0]+v1[1]*v2[1]); //returns value between 0 and pi!
+	
+	double w_angel=0.0;
+	w=w_d+w_angle
+	
+	if (x_prev.size()==1) {
+		return w_d
+		}
+	else
+		return w;
 	}
 
 //find lowest cost among options
@@ -29,12 +45,13 @@ int get_lowest(double vec[])
 //inster point pt at position idx in path
 double insert(double path[], double pt[], int idx)
 {
-	double new_path[len(path)+1];
+	const int pl=path.size();
+	double new_path[pl+1];
 	for (int k=0; k<idx; k++){
 		new_path[k]=path[k];
 		}
 	new_path[idx]=pt;
-	for (int k=idx+1; k<len(path); k++){
+	for (int k=idx+1; k<pl; k++){
 		new_path[k]=path[k-1];
 		}
 	return new_path;
@@ -44,19 +61,22 @@ double insert(double path[], double pt[], int idx)
 //output: optimized path 
 double DP(double mission[], double pos[])
 {
-	const int n=len(mission);
+	const int n=mission.size();
 	double cost=0;
 	double path[]={pos};
 	
+	//cruise velocity
+	const double v=15.5;
+	
 	for (int k=0; k<n; k++)
 	{
-		double pt=mission[k];
+		double pt[]=mission[k];
 		double options[k+1];
 		for (int i=0; i<k+1; i++){
-			double path_opt=insert(path, mission[k], i+1);
-			options[i]=0;
-			for (int j=0; j<k+2; j++){
-				options[i]+=weight(path_opt[j], path_opt[j+1])
+			double path_opt[]=insert(path, mission[k], i+1);
+			options[i]=weight(path_opt[0], path_opt[1], v, 1.0);
+			for (int j=1; j<k+2; j++){
+				options[i]+=weight(path_opt[j], path_opt[j+1], v, path_opt[j-1])
 				}
 		}
 		idx=get_lowest(options)
